522d3bd4b221be609cbc1329f33eded9db5b6641
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2020-10-17 11:55:41 -0300

    final da oficina

--- Player.js
+++ Player.js
@@ -1,6 +1,8 @@
 function Player(sprite, x, y,classename,  h, w) {
 
-    GameObject.call(this, sprite, x,y,"player", h,w);
+    this.audioLaser = new Sound("laser");
+	
+    GameObject.call(this, sprite, x, y,"player", h,w);
 
 }
 
@@ -8,5 +10,44 @@ function Player(sprite, x, y,classename,  h, w) {
 Player.prototype = Object.create(GameObject.prototype);
 
 Player.prototype.update = function () {
+	
+	if(se.teclado.CIMA ){
+		  if(this.y > 0)
+			this.y-=3;	
+	}
+	if(se.teclado.BAIXO){
+		if(this.y < canvas.width - this.h)
+			this.y+=3;
+	}
+	if(se.teclado.DIREITA){
+		 if(this.x < canvas.width - this.w)
+		this.x+=3;
+	}
+	if(se.teclado.ESQUERDA){
+		if(this.x > 0)
+			this.x-=3;
+		
+	}
+	
+	if(se.teclado.ESPACO){
+		
+		laser = new Bullet("lase1", 0,0, -4);
+		laser.setPosition( this.x + this.w/2, this.y - laser.h);
+		laser.setFire(["enemy", "kill"]);
+		
+		this.audioLaser.play();
+		
+		se.mlevel.getCurrentScene().addObjects(laser);
+		
+	}
+
+
+
 
 };
+
+
+
+Player.prototype.fired = function () {
+
+}
--- mygame.js
+++ mygame.js
@@ -4,6 +4,10 @@ const se = new StarterEngine();
 se.setResources = function () {
     //Add lista resource
     this.loader.addResource("background", "Backgrounds/purple.png", "image");
+	
+	this.loader.addResource("background2", "Backgrounds/blue.png", "image");
+	
+	
     this.loader.addResource("shipblue", "playerShip2_blue.png", "image");
     this.loader.addResource("enemyred", "enemyRed1.png", "image");
     this.loader.addResource("lase1", "laserBlue01.png", "image");
@@ -18,16 +22,51 @@ se.setResources = function () {
 
 //Quando o loading acabar
 se.gameReady = function() {
+	//criando uma cena
+	var menu = new Scene();
+	//passei a funcao incial
+	menu.setFunctionStart( setMenu );
+	
+	var jogo = new Scene();
+	jogo.setFunctionStart( setJogo );
+	
+	
+	//add cena no nivel
+	this.mlevel.addScene(menu);
+	this.mlevel.addScene(jogo);
 
 }
-
-/* //crie quantas funcoes precisar!!!
-
-function setLevel1() {
-    
-}
-
 
 function setMenu(){
 
-}*/
+	var bg = new Background("background",0 , 0, 
+	canvas.height, canvas.width);
+	
+	var logo = new GameObject("logogame", 0, 0, "gui");
+	logo.setPosition( canvas.width/2 - logo.w/2, 20);
+
+	var play = new GameObject("play", 0, 0, "gui");
+	play.setPosition( canvas.width/2 - play.w/2, 400);
+	
+	play.update = checkKeyboard;
+}
+
+//check teclado (se pressionar espaço)
+function checkKeyboard(){
+	
+	if(se.teclado.ESPACO){
+		
+		//carregando a cena 1
+		se.mlevel.loadScene(1);
+	}
+	
+}
+
+function setJogo(){
+	var bg = new Background("background2",0 , 0, 
+	canvas.height, canvas.width);
+	
+	enemy = new Enemy("enemyred", 250,0, "enemy");
+	player = new Player("shipblue", 300,600,"player");
+}
+
